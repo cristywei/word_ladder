93363e1f98db6afe99bdd9df3f879e4e4f16a6fa
Cristy Wei <cwei23@cmc.edu>
2021-02-21 13:41:29 -0800

    linter

--- word_ladder.py
+++ word_ladder.py
@@ -78,8 +78,8 @@ def verify_word_ladder(ladder):
         return False
     if len(ladder) == 1:
         return True
-    for i in range(len(ladder)-1):
-        if not _adjacent(ladder[i], ladder[i+1]):
+    for i in range(len(ladder) - 1):
+        if not _adjacent(ladder[i], ladder[i + 1]):
             return False
     return True
 
