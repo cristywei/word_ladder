7c046bb1d6a205e53f3cf0547d7bd386bd36be22
Cristy Wei <cwei23@cmc.edu>
2021-02-21 13:33:04 -0800

    update code

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,5 @@
-#!/bin/python3
-
+#w!/bin/python3
+from collections import deque
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
@@ -28,6 +28,35 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    if start_word == end_word:  #same word
+        return [end_word]
+
+    if _adjacent(start_word, end_word): #can create word ladder with starter words
+        return [start_word, end_word]
+
+    f = open(dictionary_file, "r").read().split('\n')
+    dictionary = []
+    for word in f:
+        dictionary += [word]
+
+    stack = []
+    stack.append(start_word)
+    queue = deque([])
+    queue.append(stack)
+
+    while len(queue) != 0:
+        dict_copy = list(dictionary)    #cant iterate and remove from same list
+        ladder = queue.popleft()
+        for dict_word in dict_copy:
+            if _adjacent(ladder[-1], dict_word):
+                if dict_word == end_word:
+                    ladder.append(dict_word)
+                    return ladder
+                stackcopy = list(ladder)
+                stackcopy.append(dict_word)
+                queue.append(stackcopy)
+                dictionary.remove(dict_word)
+    return None
 
 
 def verify_word_ladder(ladder):
@@ -40,7 +69,14 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
-
+    if len(ladder) == 0:
+        return False
+    if len(ladder) == 1:
+        return True
+    for i in range(len(ladder)-1):
+        if not  _adjacent(ladder[i], ladder[i+1]):
+            return False
+    return True
 
 def _adjacent(word1, word2):
     '''
@@ -52,3 +88,10 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    diff = 0
+    if len(word1) != len(word2):
+        return False
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            diff += 1
+    return diff == 1
